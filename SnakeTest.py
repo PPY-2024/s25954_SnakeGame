015497fecb9815af63fe0949654200af31abf742
MyronenkoHeorhii <s25954@pjwstk.edu.pl>
2024-05-05 22:43:57 +0200

    Created and configured DockerFile.

--- SnakeTest.py
+++ SnakeTest.py
@@ -1,38 +1,20 @@
-import unittest
-import pygame as pg
-from snake_game import SnakeGame
+import pytest
+import io
+import sys
 
-class TestSnakeGame(unittest.TestCase):
-    def test_snake_movement(self):
+from snake_game.SnakeGame import *
+"""
 
-        game = SnakeGame(5, 100)
-        initial_position = game.snake.center
-        game.handle_event(pg.K_w)  # Move snake up
-        self.assertEqual(game.snake.center, (initial_position[0], initial_position[1] - 100))
-        game.handle_event(pg.K_s)  # Move snake down
-        self.assertEqual(game.snake.center, initial_position)
+def lose_condition():
+    initialize_with_atr(10, "Player", 3)
+    snake.right = 501
+    assert score == 0
 
-    def test_food_generation(self):
-        game = SnakeGame(5, 100)
-        food_positions = set()
-        for _ in range(100):
-            game.generate_food()
-            food_positions.add(game.food.center)
-        self.assertTrue(len(food_positions) > 1)
 
-    def test_game_over(self):
-
-        game = SnakeGame(5, 100)
-        game.snake.center = (0, 0)
-        game.handle_event(pg.K_d)
-        game.update_game()
-        self.assertTrue(game.game_over)
-
-        game.reset_game()
-        game.snake.center = (250, 250)
-        game.handle_event(pg.K_a)
-        game.update_game()
-        self.assertFalse(game.game_over)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_initialize_atr():
+    input_size = 4
+    player_name = "Player"
+    speed = 3
+    initialize_with_atr(input_size, player_name, speed)
+    assert field_size != 0
+"""
--- snake_game/SnakeGame.py
+++ snake_game/SnakeGame.py
@@ -5,15 +5,55 @@ pg.font.init()
 
 MIN_SIZE = 5
 MAX_SIZE = 25
+WINDOW = 500
 
 field_size = 0
+user_name = ""
+snake_speed = 0
+
 while field_size < MIN_SIZE or field_size > MAX_SIZE:
     field_size = int(input(f"Set field size (between {MIN_SIZE}x{MIN_SIZE} and {MAX_SIZE}x{MAX_SIZE}): "))
 
-WINDOW = 500
+user_name = input("Your name: ")
+while snake_speed < 1 or snake_speed > 10:
+    snake_speed = int(input("Set snake speed: "))
+
 TILE_SIZE = WINDOW // field_size
 RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
-get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+
+"""
+TILE_SIZE = 0
+RANGE = [0, 1]
+
+
+def initialize():
+    global field_size, user_name, snake_speed, TILE_SIZE, RANGE
+
+
+#
+
+def initialize_with_atr(input_field_size, input_user_name, input_snake_speed):
+    global field_size, user_name, snake_speed, WINDOW, RANGE, TILE_SIZE
+
+    while True:
+        if input_field_size < MIN_SIZE or input_field_size > MAX_SIZE:
+            print("field size out of allowed range")
+            return 0
+        else:
+            field_size = input_field_size
+            user_name = input_user_name
+            snake_speed = input_snake_speed
+
+            TILE_SIZE = WINDOW // field_size
+            RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+            break
+"""
+
+def get_random_position():
+    global RANGE
+    return [randrange(*RANGE), randrange(*RANGE)]
+
+
 snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
@@ -31,74 +71,80 @@ color = (255, 255, 255)
 font_size = 24
 font = "Alias"
 
-user_name = input("Your name: ")
-snake_speed = 0
-while snake_speed < 1 or snake_speed > 10:
-    snake_speed = int(input("Set snake speed: "))
 
-
-def show_score(color, font, size):
-    score_font = pg.font.SysFont(font, size)
-    score_surface = score_font.render(user_name + "'s Score : " + str(score) + " / Best score: " + str(record_score),
-                                      True, color)
+def show_score(font_color, font_style, size, input_user_name):
+    score_font = pg.font.SysFont(font_style, size)
+    score_surface = score_font.render(
+        input_user_name + "'s Score : " + str(score) + " / Best score: " + str(record_score),
+        True, font_color)
     score_rect = score_surface.get_rect()
     screen.blit(score_surface, score_rect)
 
 
-def lost_score(color, font, size):
-    lost_font = pg.font.SysFont(font, size)
+def lost_score(font_color, font_style, size):
+    global score, record_score, snake, food, segments, length, snake_dir
+
+    if record_score < score:
+        record_score = score
+
+    lost_font = pg.font.SysFont(font_style, size)
     lost_surface = lost_font.render("You lost. Your score: " + str(score) + "\n" + "Best score: " + str(record_score),
-                                    True, color)
+                                    True, font_color)
     lost_rect = lost_surface.get_rect(center=(WINDOW // 2, WINDOW // 2))
     screen.blit(lost_surface, lost_rect)
 
-
-while True:
-    # show_score(color, "Alias", font_size)
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            exit()
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_w and dirs[pg.K_w]:
-                snake_dir = (0, -TILE_SIZE)
-                dirs = {pg.K_w: 1, pg.K_s: 0, pg.K_a: 1, pg.K_d: 1}
-            if event.key == pg.K_s and dirs[pg.K_s]:
-                snake_dir = (0, TILE_SIZE)
-                dirs = {pg.K_w: 0, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
-            if event.key == pg.K_a and dirs[pg.K_a]:
-                snake_dir = (-TILE_SIZE, 0)
-                dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 0}
-            if event.key == pg.K_d and dirs[pg.K_d]:
-                snake_dir = (TILE_SIZE, 0)
-                dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 0, pg.K_d: 1}
-    screen.fill('black')
-    if snake.center == food.center:
-        food.center = get_random_position()
-        length += 1
-        score += 1
-    self_eating = pg.Rect.collidelist(snake, segments[:-1]) != -1
-    if snake.left < 0 or snake.right > WINDOW or snake.top < 0 or snake.bottom > WINDOW or self_eating:
-        # print("You lost. Here is your score:", score)
-
-        if (record_score < score):
-            record_score = score
-
-        lost_score(color, font, font_size)
-        score = 0
-        pg.display.flip()
-        lost_score(color, font, font_size)
-        pg.time.wait(2000)
-
-        snake.center, food.center = get_random_position(), get_random_position()
-        length, snake_dir = 1, (0, 0)
-        segments = [snake.copy()]
-
-    pg.draw.rect(screen, 'red', food)
-    [pg.draw.rect(screen, 'green', segment) for segment in segments]
-
-    show_score(color, font, font_size)
-    snake.move_ip(snake_dir)
-    segments.append(snake.copy())
-    segments = segments[-length:]
     pg.display.flip()
-    clock.tick(snake_speed)
+
+    score = 0
+
+    snake.center, food.center = get_random_position(), get_random_position()
+    length, snake_dir = 1, (0, 0)
+    segments = [snake.copy()]
+
+
+def game(input_user_name):
+    global score, segments, screen, record_score, length, \
+        snake, snake_dir, snake_speed, food, font, \
+        color, clock, dirs, WINDOW, MAX_SIZE, \
+        MIN_SIZE, TILE_SIZE, RANGE, get_random_position
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_w and dirs[pg.K_w]:
+                    snake_dir = (0, -TILE_SIZE)
+                    dirs = {pg.K_w: 1, pg.K_s: 0, pg.K_a: 1, pg.K_d: 1}
+                if event.key == pg.K_s and dirs[pg.K_s]:
+                    snake_dir = (0, TILE_SIZE)
+                    dirs = {pg.K_w: 0, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
+                if event.key == pg.K_a and dirs[pg.K_a]:
+                    snake_dir = (-TILE_SIZE, 0)
+                    dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 0}
+                if event.key == pg.K_d and dirs[pg.K_d]:
+                    snake_dir = (TILE_SIZE, 0)
+                    dirs = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 0, pg.K_d: 1}
+        screen.fill('black')
+        if snake.center == food.center:
+            food.center = get_random_position()
+            length += 1
+            score += 1
+        self_eating = pg.Rect.collidelist(snake, segments[:-1]) != -1
+        if snake.left < 0 or snake.right > WINDOW or snake.top < 0 or snake.bottom > WINDOW or self_eating:
+            lost_score(color, font, font_size)
+            pg.time.wait(2000)
+
+        pg.draw.rect(screen, 'red', food)
+        [pg.draw.rect(screen, 'green', segment) for segment in segments]
+
+        show_score(color, font, font_size, input_user_name)
+        snake.move_ip(snake_dir)
+        segments.append(snake.copy())
+        segments = segments[-length:]
+        pg.display.flip()
+        clock.tick(snake_speed)
+
+
+#initialize()
+game(user_name)
